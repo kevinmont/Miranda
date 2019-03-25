db9f82c1f83c472fc4968f6382d42a4826c05ffb
kevinmont <live-mont@outlook.com>
2019-03-25 14:58:39 -0600

    Se agrego el servicio de recuperacion de productos, falta aplicar dependencia de iniciar sesion

---
+++ controllers/ProductosController.php
@@ -0,0 +1,52 @@
+<?php
+
+include_once "../models/Productos.php";
+include_once "../models/Usuario.php";
+session_start();
+
+// Si no existe session
+// no se muestra el precion y los estampados
+$response = '{';
+$error = -1;
+$oProduct = new Producto();
+// if (isset($_SESSION['user']) && !empty($_SESSION['user'])) {
+$productos = $oProduct->getProducts();
+
+if (!empty($productos)) {
+    $response .= '"status": 200,
+        "products" : [';
+    foreach ($productos as $producto) {
+        $response .= '{
+                "id": "' . $producto->getIdProducto() . '",
+                "nombre": "' . $producto->getNombre() . '",
+                "descripcion": "' . $producto->getDescripcion() . '",
+                "imagen": "' . $producto->getImagen() . '",
+                "estilo": "' . $producto->getEstilo() . '",
+                "material": "' . $producto->getMaterial() . '",
+                "estampado": "' . $producto->getEstampado() . '",
+                "color": ""
+            },';
+    }
+    $response = substr($response, 0, strlen($response) - 1);
+    $response .= ']}';
+} else {
+    $error = "No existen productos";
+}
+// podemos buscar todos los productos que no
+// } else {
+//     $error = 1; // Ha ocurrido un error;
+//     // no podemos visualizar los datos de precio y material
+// }
+if ($error !== -1) {
+    $response = '{
+        "status": "204",
+        "message": "No content",
+        "body": {
+            "error": "' . $error . '"
+        }
+    }';
+}
+header('Content-Type: application/json');
+echo $response;
+
+// de lo contrario el json debera
---
+++ models/Usuario.php
@@ -0,0 +1,151 @@
+<?php
+
+error_reporting(E_ALL);
+include_once "AccesoDatos.php";
+abstract class Usuario
+{
+
+    protected $idUsuario = -1;
+    protected $nombre = "";
+    protected $aPaterno = "";
+    protected $aMaterno = "";
+    protected $email = "";
+    protected $contraseña = "";
+
+    public function setIdUsuario($idUsuario)
+    {
+        $this->idUsuario = $idUsuario;
+    }
+
+    public function getIdUsuario()
+    {
+        return $this->idUsuario;
+    }
+
+    public function setNombre($nombre)
+    {
+        $this->nombre = $nombre;
+    }
+
+    public function getNombre()
+    {
+        return $this->nombre;
+    }
+
+    public function setAPaterno($aPaterno)
+    {
+        $this->aPaterno = $aPaterno;
+    }
+
+    public function getAPaterno()
+    {
+        return $this->aPaterno;
+    }
+
+    public function setAMaterno($aMaterno)
+    {
+        $this->aMaterno = $aMaterno;
+    }
+
+    public function getAMaterno()
+    {
+        return $this->aMaterno;
+    }
+
+    public function setEmail($email)
+    {
+        $this->email = $email;
+    }
+
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    public function setContraseña($contraseña)
+    {
+        $this->contraseña = $contraseña;
+    }
+
+    public function getContraseña()
+    {
+        return $this->contraseña;
+    }
+
+    public function getNombreCompleto()
+    {
+        return $this->nombre . " " . $this->aPaterno . " " . $this->aMaterno;
+    }
+
+    protected function buscarPorEmailContraseña()
+    {
+        $oAccesoDatos = new AccesoDatos();
+        $sQuery = "";
+        $aResultSet = null;
+        $bUserExist = false;
+        if ($this->email == -1 || $this->contraseña == "") {
+            throw new Exception("Usuario->buscarPorEmailContraseña(): faltan datos");
+        } else {
+            if ($oAccesoDatos->conectar()) {
+                $sQuery = "SELECT id_usuario, nombre, a_paterno, a_materno
+					FROM usuarios
+					WHERE email = " . $this->email . "
+					AND contrasenia = '" . $this->contraseña . "'";
+                $aResultSet = $oAccesoDatos->ejecutarConsulta($sQuery);
+                $oAccesoDatos->desconectar();
+                if ($aResultSet) {
+                    $this->idUsuario = $aResultSet[0][0];
+                    $this->nombre = $aResultSet[0][1];
+                    $this->aPaterno = $aResultSet[0][2];
+                    $this->aMaterno = $aResultSet[0][3];
+                    $bUserExist = true;
+                }
+            }
+        }
+        return $bUserExist;
+    }
+
+    /*Devuelve la cadena de inserción (para reutilización en herencia)*/
+    protected function getInsertar()
+    {
+        $sQuery = "";
+        if ($this->email == "" || $this->contraseña == "" || $this->nombre == "") {
+            throw new Exception("Usuario->getInsertar(): faltan datos");
+        } else {
+            $sQuery = "INSERT INTO usuarios (nombre, a_paterno, a_materno, email, contrasenia)
+				VALUES (" . $this->nombre . ", '" . $this->aPaterno . "',
+				'" . $this->aMaterno . "', '" . $this->email . "',
+				'" . $this->contraseña . "');";
+        }
+        return $sQuery;
+    }
+
+    /*Devuelve la cadena de modificación (para reutilización en herencia)*/
+    protected function getModificar()
+    {
+        $sQuery = "";
+        if ($this->idUsuario == -1 || $this->sContrasenia == "" || $this->sNombre == "") {
+            throw new Exception("Usuario->getModificar(): faltan datos");
+        } else {
+            $sQuery = "UPDATE usuario
+				SET contrasenia= '" . $this->contrasenia . "',
+				nombre= '" . $this->nombre . "' ,
+				a_paterno= '" . $this->aPaterno . "' ,
+				a_materno= '" . $this->aMaterno . "')
+                WHERE id_usuario = " . $this->idUsuario . ";";
+        }
+        return $sQuery;
+    }
+
+    /*Devuelve la cadena de eliminación (para reutilización en herencia)*/
+    protected function getBorrar()
+    {
+        $sQuery = "";
+        if ($this->idUsuario == -1) {
+            throw new Exception("Usuario->getBorrar(): faltan datos");
+        } else {
+            $sQuery = "DELETE FROM usuario WHERE nCveUsu = " . $this->idUsuario . "; ";
+        }
+        return $sQuery;
+    }
+}
